6e890a0694f51db0be3f8a714e44de2a15fc7013
Grant Shell <grantshell33@icloud.com>
2021-08-09 11:21:29 -0500

    models and main handlebar

---
+++ models/comment.js
@@ -0,0 +1,54 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+const User = require("./User");
+
+class Comment extends Model {
+  async getCommentUserName(id) {
+    const userData = await User.findByPk(id);
+    console.log("userData :>> ", userData);
+    return userData;
+  }
+}
+
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    body: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "post",
+        key: "id",
+      },
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "comment",
+  }
+);
+
+module.exports = Comment;
